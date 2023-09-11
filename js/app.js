28019daef7a5ad25166832da5a48b62e607cf0c5
chika457 <chikabv1@gmail.com>
2023-09-11 22:48:46 +0600

    ASassiNS

---
+++ js/app.js
@@ -0,0 +1,12 @@
+new Swiper('.slider', {
+  // direction: 'vertical',
+  // freeMode: true,
+  speed: 2400,
+  mousewheel: {
+    enabled: true,
+    sevsitivity: 2.4
+  },
+	parallax: true,
+	spaceBetween: 18,
+  parallax: true,
+})
